0e5afcd7af839318d2b524f79855ebc01f2e83d0
benlyna <106200222+benlyna@users.noreply.github.com>
2022-06-01 16:09:15 +0100

    Update Projet.py

--- Projet.py
+++ Projet.py
@@ -6,20 +6,20 @@ import os
 
 print  ("--------------- Crypter un fichier avec sha 256 ---------------" )
 
-choix = input("voulez vous crypter ou décripter un fichier c-> crypté , d-> decrypté :")
+choix = input("Voulez vous crypter ou décripter un fichier c-> crypté , d-> decrypté :")
  
 if choix == "c":
 
-	entree = input("entrez le nom du fichier a chifrer/dechifrer :")
+	entree = input("Entrez le nom du fichier a chiffrer :")
 	if os.path.exists(entree): 
-		avis = input ( " y -> pour canger le contenue du fichier , n -> pour chifrer le fichier sans changement :")
+		avis = input ( " y-> pour changer le contenue du fichier , n-> pour chiffrer le fichier sans changement :")
 		if avis == "y" : 
-			contenue = input ("ecrire le comptenue du fichier :  ")
+			contenu = input ("Ecrire le contenu du fichier :  ")
 			fichier = open (entree,'w')
-			fichier.write(contenue )
+			fichier.write(contenu )
 			fichier.close()
 		elif avis == "n" : 
-			print ("Le contenue du fichier ne change pas : ")
+			print ("Le contenu du fichier ne change pas : ")
 			with open (entree ,'r') as f :
 				for ligne in  f :
 					print (ligne)
@@ -27,8 +27,8 @@ if choix == "c":
 		else : 
 			print  ("Erreur")
 
-		sortie =input("entrez le nom du fichoier final (crypter) :") 
-		key = input ("Donnez la clé : ")
+		sortie =input("Entrez le nom du fichier final (crypté) :") 
+		key = input ("Donnez la clé de chiffrement : ")
 		keys = sha256(key.encode('utf-8')).digest()
 
 		with open(entree,'rb') as f_entree:
@@ -45,11 +45,11 @@ if choix == "c":
 		print  ("Ce fichier n'existe pas! ")
 			
 elif choix == "d":
-	entree = input("entrez le nom du fichier dechifrer :")
+	entree = input("Entrez le nom du fichier chiffré :")
 	if os.path.exists(entree): 
 	
-		sortie =input("entrez le nom du fichoier final (décrypter) :") 
-		key = input ("Donnez la clé (La clé dois etre la meme que celle du décriptage) : ")
+		sortie =input("entrez le nom du fichier final (décrypté) :") 
+		key = input ("Donnez la clé de déchiffrement (La clé doit etre la meme que celle du décriptage) : ")
 		keys = sha256(key.encode('utf-8')).digest()
 	
 		with open(entree,'rb') as f_entree:
